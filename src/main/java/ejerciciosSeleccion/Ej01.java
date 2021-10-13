38b29417b4c437d68ba3d5bfb60e9d81ef145712
CrisJimenez00 <cristina.del.carmen.jimenez.1@gmail.com>
2021-10-13 14:13:41 +0200

    Primeros dos ejercicios

---
+++ src/main/java/ejerciciosSeleccion/Ej01.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosSeleccion;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Cris
+ */
+public class Ej01 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("escanear edad:");
+
+        int edad = scanner.nextInt();
+
+        if (edad >= 18) {
+            System.out.println("Es mayor de edad");
+        } else {
+            System.out.println("No es mayor de edad");
+        }
+
+        System.out.print("mostrar la edad: ");
+        System.out.println(edad);
+    }
+    
+    //Resultados:
+    //-Si metemos 10: No es mayor de edad
+    //-Si metemos 18: Es mayor de edad
+    //-Si metemos 67: Es mayor de edad
+
+}
