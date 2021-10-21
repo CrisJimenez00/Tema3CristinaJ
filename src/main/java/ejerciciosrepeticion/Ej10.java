004758123e88232eef2c74a24aa08ec53a07d28f
CrisJimenez00 <cristina.del.carmen.jimenez.1@gmail.com>
2021-10-21 12:37:00 +0200

    Ej10 subido

---
+++ src/main/java/ejerciciosrepeticion/Ej10.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejerciciosrepeticion;
+
+/**
+ *
+ * @author cristina
+ */
+public class Ej10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Implementa un programa Java que permita visualizar 
+        //todas las letras minúsculas, desde la 'a' hasta la 'z', usando un for.
+        for (int i = 65; i < 91; i++) {
+            char letra = (char) i;
+            System.out.println(letra);
+        }
+    }
+
+}
