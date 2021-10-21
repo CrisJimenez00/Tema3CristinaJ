07b0affa26c9b2691aefb223b1325fb14b674152
CrisJimenez00 <cristina.del.carmen.jimenez.1@gmail.com>
2021-10-21 12:07:49 +0200

    Ejercicios de repetición copiados

---
+++ src/main/java/ejerciciosrepeticion/Ej01.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejerciciosrepeticion;
+
+/**
+ *
+ * @author cristina
+ */
+public class Ej01 {
+
+    public static void main(String[] args) {
+        int x = 1;
+
+        while (x <= 100) {
+            System.out.print(x);
+            System.out.print(" - ");
+            x = x + 1;
+        }
+    }
+
+}
