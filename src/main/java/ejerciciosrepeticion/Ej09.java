5dd3393cec7f7459eb7fc878b46a170effcdf403
CrisJimenez00 <cristina.del.carmen.jimenez.1@gmail.com>
2021-10-21 12:36:43 +0200

    Ej09 subido

---
+++ src/main/java/ejerciciosrepeticion/Ej09.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejerciciosrepeticion;
+
+/**
+ *
+ * @author cristina
+ */
+public class Ej09 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 0;
+        int j = 6;
+        while (i < 0) {
+            i++;
+            j++;
+        }
+        System.out.println(j);
+
+    }
+
+}
