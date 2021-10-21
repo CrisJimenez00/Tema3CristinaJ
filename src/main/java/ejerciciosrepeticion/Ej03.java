0d789e29c1238fead4275bb1f8d6d17b26215a27
CrisJimenez00 <cristina.del.carmen.jimenez.1@gmail.com>
2021-10-21 12:35:52 +0200

    Ej03 subido

---
+++ src/main/java/ejerciciosrepeticion/Ej03.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejerciciosrepeticion;
+
+/**
+ *
+ * @author cristina
+ */
+public class Ej03 {
+
+    public static void main(String[] args) {
+        int x = 15;
+
+        for (int i = 1; i < 25; i++) {
+            System.out.print(x);
+            System.out.print(" - ");
+            x = x * 2;
+        }
+
+    }
+
+}
