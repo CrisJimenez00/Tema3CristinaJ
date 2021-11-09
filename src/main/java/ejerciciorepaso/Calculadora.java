2da13229d071a63c571e7002f9cd08e3c3ed7988
CrisJimenez00 <cristina.del.carmen.jimenez.1@gmail.com>
2021-11-09 14:41:17 +0100

    Se comprueban condiciones

--- src/main/java/ejerciciorepaso/Calculadora.java
+++ src/main/java/ejerciciorepaso/Calculadora.java
@@ -16,6 +16,7 @@ public class Calculadora {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
+        int operando1 = 0, operando2 = 0;
         //Variable centinela
         boolean solicitarDatos = true;
 
@@ -25,23 +26,26 @@ public class Calculadora {
         //Pedimos a los usuarios los datos correspondientes y controlamos excepciones
         System.out.println("Introduce dos operandos");
         do {
-            try {
-                //Bloque candidato a lanzar la excepcion
-                //(Podemos hacer el try catch a cada uno, pero esto es para repaso simple)
-                System.out.println("Operando 1: ");
-                int numero1 = teclado.nextInt();
-                System.out.println("Operando 2: ");
-                int numero2 = teclado.nextInt();
+            do {
+                try {
+                    //Bloque candidato a lanzar la excepcion
+                    //(Podemos hacer el try catch a cada uno, pero esto es para repaso simple)
+                    System.out.println("Operando 1: ");
+                    operando1 = teclado.nextInt();
+                    System.out.println("Operando 2: ");
+                    operando2 = teclado.nextInt();
 
-                //Para controlar que los datos están correctamente introducidos.
-                solicitarDatos = false;
-            } catch (InputMismatchException ime) {
-                //Código para tratar el error
-                System.out.println("Se ha introducido texto en lugar de números"
-                        + "\nVuelva a introducir los datos");
-                teclado.nextLine();
-            }
-        } while (!solicitarDatos);
+                    //Para controlar que los datos están correctamente introducidos.
+                    solicitarDatos = false;
+                } catch (InputMismatchException ime) {
+
+                    //Código para tratar el error
+                    System.out.println("Se ha introducido texto en lugar de números"
+                            + "\nVuelva a introducir los datos");
+                    teclado.nextLine();
+                }
+            } while (solicitarDatos); // Bucle que controla la excepción
+        } while (!((operando1 > -100 && operando1 < 100) && (operando2 > -100 && operando2 < 100)));
         //Mostrar menú de opciones(Solo se sale si el usuario lo pide)
         //Filtramos las entradas del usuario(controlamos excepciones)
         //Cuatro opciones: +,-,*,/
