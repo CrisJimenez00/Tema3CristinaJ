96e7ab6c120ba46851dca81f2aefb47b43146cb9
CrisJimenez00 <cristina.del.carmen.jimenez.1@gmail.com>
2021-11-10 13:44:25 +0100

    Modularización del código

--- src/main/java/ejerciciorepaso/Calculadora.java
+++ src/main/java/ejerciciorepaso/Calculadora.java
@@ -12,6 +12,27 @@ import java.util.Scanner;
  * @author cristina
  */
 public class Calculadora {
+    
+    //Método suma. Solo se utiliza en la clase Calculadora
+    //Se le pone privado porque solo se usa en esta clase
+    //Recibe dos parámetros y devuelve un entero
+    //Con el resultado de la suma de los parámetros
+    private static int sumar (int a, int b){
+        int resultado = a + b;
+        return resultado;
+    }
+    private static int restar (int a, int b){
+        int resultado = a - b;
+        return resultado;
+    }
+    private static int multiplicar (int a, int b){
+        int resultado = a * b;
+        return resultado;
+    }
+    private static int dividir (int a, int b){
+        int resultado = a / b;
+        return resultado;
+    }
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
@@ -19,15 +40,17 @@ public class Calculadora {
         int operando1 = 0, operando2 = 0;
         //Variable centinela
         boolean solicitarDatos = true;
+        String opcion;
 
         //Comienzo del programa
         System.out.println("Bienvenido al calculator");
 
-        //Pedimos a los usuarios los datos correspondientes y controlamos excepciones
-        System.out.println("Introduce dos operandos");
         do {
+            //Pedimos a los usuarios los datos correspondientes y controlamos excepciones
+            System.out.println("Introduce dos operandos");
             do {
                 try {
+                    solicitarDatos = true;
                     //Bloque candidato a lanzar la excepcion
                     //(Podemos hacer el try catch a cada uno, pero esto es para repaso simple)
                     System.out.println("Operando 1: ");
@@ -46,11 +69,46 @@ public class Calculadora {
                 }
             } while (solicitarDatos); // Bucle que controla la excepción
         } while (!((operando1 > -100 && operando1 < 100) && (operando2 > -100 && operando2 < 100)));
-        //Mostrar menú de opciones(Solo se sale si el usuario lo pide)
-        //Filtramos las entradas del usuario(controlamos excepciones)
-        //Cuatro opciones: +,-,*,/
-        //No sabes cuántos operandos exactos tiene
 
+        teclado.nextLine();
+        int resultado = 0;
+        do {
+            System.out.println("Opciones del programa"
+                    + "\n1.Sumar"
+                    + "\n2.Restar"
+                    + "\n3.Multiplicar"
+                    + "\n4.Dividir"
+                    + "\n5.Salir");
+
+            opcion = teclado.nextLine();
+
+            switch(opcion){
+                case "1":
+                    resultado = sumar(operando1, operando2);
+                    break;
+                    
+                case "2":
+                    resultado = restar(operando1, operando2);
+                    break;
+                    
+                case "3":
+                    resultado = multiplicar(operando1, operando2);
+                    break;
+                    
+                case "4":
+                    resultado = dividir(operando1, operando2);
+                    break;
+                    
+                case "5":
+                        System.out.println("Hasta la próxima");
+                    break;
+                    
+                default:
+                        System.out.println("Introduce un número correcto");
+                        
+            }
+            System.out.println("El resultado es: " + resultado);
+        } while (!opcion.equals("5"));
     }
 
 }
